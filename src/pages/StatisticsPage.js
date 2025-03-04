f6de96a702eee928033d43cac3f95685709b68e8
Rudenko Dmitry <rudenko.d@rt.ru>
2025-03-04 15:11:18 +0300

    close #10

--- src/pages/StatisticsPage.js
+++ src/pages/StatisticsPage.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { Box, Typography, Link  } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom'; // Импортируем Link для маршрутизации
+import { Box, Typography, Link, useTheme } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
 const StatisticsPage = () => {
   const [games, setGamesData] = useState([]);
+  const theme = useTheme(); // Получаем текущую тему
 
   useEffect(() => {
-    // Загружаем данные постов
     fetch(`${process.env.PUBLIC_URL}/data/posts.json`)
       .then((response) => response.json())
       .then((posts) => {
-        // Создаем объект для подсчета количества партий по играм
         const playsCount = {};
 
-        // Проходим по всем постам и считаем партии
         posts.forEach((post) => {
           post.games.forEach((game) => {
             if (playsCount[game.title]) {
@@ -24,7 +23,6 @@ const StatisticsPage = () => {
           });
         });
 
-        // Преобразуем объект в массив для графика
         const formattedData = Object.keys(playsCount).map((gameName) => ({
           name: gameName,
           plays: playsCount[gameName],
@@ -34,6 +32,32 @@ const StatisticsPage = () => {
       });
   }, []);
 
+  // Кастомный компонент для столбцов
+  const CustomBar = (props) => {
+    const { x, y, width, height, name } = props;
+
+    return (
+      <Link
+        component={RouterLink}
+        to={`/game/${name}`}
+        sx={{ color: 'inherit', textDecoration: 'none' }}
+      >
+        <g>
+          <rect x={x} y={y} width={width} height={height} fill={theme.palette.info.main} />
+          <text
+            x={x}
+            y={y + height / 2}
+            dy={0}
+            textAnchor="start"
+            fill={theme.palette.text.primary}
+            fontSize={12}
+          >
+            {name}
+          </text>
+        </g>
+      </Link>
+    );
+  };
 
   return (
     <Box sx={{ p: 3 }}>
@@ -41,39 +65,28 @@ const StatisticsPage = () => {
         Статистика
       </Typography>
 
-      {/* График */}
       <Box sx={{ width: '100%', height: 2500 }}>
         <ResponsiveContainer>
           <BarChart
             data={games}
-            layout="vertical" // Вертикальный график
+            layout="vertical"
             margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis type="number" /> {/* Ось X: количество партий */}
-            {/* <YAxis type="category" dataKey="name" width={150} /> Ось Y: названия игр */}
+            <XAxis type="number" />
             <YAxis
               type="category"
               dataKey="name"
-              width={250}
-              tick={(props) => {
-                const { x, y, payload } = props;
-                return (
-                  <Link
-                    component={RouterLink}
-                    to={`/game/${payload.value}`} // Ссылка на страницу игры
-                    sx={{ color: 'inherit', textDecoration: 'none' }}
-                  >
-                    <text  x={x} y={y} dy={5} textAnchor="end" fill="currentColor">
-                      {payload.value}
-                    </text>
-                  </Link>
-                );
-              }}
-            /> {/* Ось Y: названия игр */}
+              width={0}
+              tick={{ fontSize: 12 }}
+            />
             <Tooltip />
             <Legend />
-            <Bar dataKey="plays" fill="#8884d8" name={'Колличество партий'} /> {/* Столбцы для количества партий */}
+            <Bar
+              dataKey="plays"
+              name="Количество партий"
+              shape={<CustomBar />}
+            />
           </BarChart>
         </ResponsiveContainer>
       </Box>
